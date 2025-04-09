e2bbd86bf61785bc0229cc3e6443d29eb9c9e7c5
athalafk <athalafarastya@gmail.com>
2025-04-09 15:43:16 +0700

    Adding middleware verify

--- routes/riwayatRoutes.js
+++ routes/riwayatRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { getRiwayat } = require("../controllers/riwayatController");
+const { authMiddleware, authorizeRoles } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", getRiwayat);
+router.use(authMiddleware);
+
+router.get("/", authorizeRoles("admin"), getRiwayat);
 
 module.exports = router;
